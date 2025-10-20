d1cea4976090d3a3ece25dd207f92585a59b8d11
Cristopher Roman <169313807+CristopherRoman@users.noreply.github.com>
2025-10-19 21:34:39 -0300

    Breacking changes

--- Funciones.js
+++ Funciones.js
@@ -1,1 +1,19 @@
 // Archivo con funcionalidades extras
+
+
+let prevScrollpos = window.pageYOffset;
+console.log(prevScrollpos)
+const navbar = document.getElementById("navbar-principal");
+
+window.onscroll = function () {
+    let currentScrollPos = window.pageYOffset;
+
+    if (prevScrollpos > currentScrollPos) {
+        // Scroll hacia arriba: mostrar navbar
+        navbar.classList.remove("navbar-hidden")
+    } else {
+        navbar.classList.add("navbar-hidden")
+    }
+
+    prevScrollpos = currentScrollPos;
+};
